e343dcfbe74c13d681292628ac625b4e2034831a
sauhaard6003 <78654368+sauhaard6003@users.noreply.github.com>
2023-04-10 23:58:31 +0530

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,41 @@
+const btn=document.querySelector('.changeColorBtn');
+
+const colorGrid = document.querySelector('.colorGrid');
+
+const colorValue = document.querySelector('.colorValue');
+btn.addEventListener('click',async ()=>{
+    const color = chrome.storage.sync.get('color',({color})=>{
+        console.log('color: ',color);
+    });
+    let [tab] = await chrome.tabs.query({active:true,currentWindow:true});
+    chrome.scripting.executeScript({
+        target: {tabId:tab.id},
+        function: pickColor,
+    },async (injectionResults)=>{
+        const [data]=injectionResults;
+        if (data.result){
+            const color=data.result.sRGBHex;
+            colorGrid.style.backgroundColor = color;
+            colorValue.innerText = color;
+            try{
+                await navigator.clipboard.writeText(color);
+            }catch(err){
+                console.log(err);
+            }
+        }
+    });
+});
+
+const pickColor = async() => {
+    try{
+        const eyeDropper = new EyeDropper();
+
+        return await eyeDropper.open();
+
+        
+    }
+    catch (err){
+        console.log(err);
+    }
+}
+
